5b3cd0379ff9667e615722eeb567288ab185bccf
natandavinci <natanaelqueiroz969@gmail.com>
2026-06-17 08:18:31 -0300

    Created the new pydantic

--- email.py
+++ email.py
@@ -1,5 +1,17 @@
-from typing import Optional
+from typing import (
+    Optional,
+    Literal)
 from typing_extensions import TypedDict
+from google import genai
+import os
+from dotenv import load_dotenv
+from pydantic import BaseModel, Field
+
+load_dotenv()
+
+client = genai.Client(
+    api_key=os.getenv("GOOGLE_API_KEY")
+)
 
 # State
 class GraphState(TypedDict):
@@ -12,9 +24,25 @@ class GraphState(TypedDict):
 
     final_answer: Optional[str]
 
+class destination_sector(BaseModel):
+    sector: Literal["Finance", "Support", "Commercial"] = Field(
+        description=" The destination sector base on the email content "
+    )
+
 # Node-1
 def classify_email(state: GraphState) -> dict:
-    pass
+
+    body_email = state["body_email"]
+
+    prompt = f"Classifique o seguinte e-mail em um dos setores válidos: {body_email}"
+    
+    response = client.models.generate_content(
+        model="gemini-2.5-flah",
+        contents=prompt
+    )
+    
+
+    return {"destination_sector": response.destination_sector}
 
 # Node-2
 def  answer_finance(state: GraphState) -> dict:
