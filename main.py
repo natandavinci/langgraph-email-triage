17daba6f657232fec2bd01dc213df19715e69e9e
natandavinci <natanaelqueiroz969@gmail.com>
2026-06-17 08:39:08 -0300

    feat: node-1 working

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ from typing import (
     Literal)
 from typing_extensions import TypedDict
 from google import genai
+from google.genai import types
 import os
 from dotenv import load_dotenv
 from pydantic import BaseModel, Field
+import json
 
 load_dotenv()
+
 
 client = genai.Client(
     api_key=os.getenv("GOOGLE_API_KEY")
@@ -24,7 +27,7 @@ class GraphState(TypedDict):
 
     final_answer: Optional[str]
 
-class destination_sector(BaseModel):
+class EmailClassification(BaseModel):
     sector: Literal["Finance", "Support", "Commercial"] = Field(
         description=" The destination sector base on the email content "
     )
@@ -37,12 +40,18 @@ def classify_email(state: GraphState) -> dict:
     prompt = f"Classifique o seguinte e-mail em um dos setores válidos: {body_email}"
     
     response = client.models.generate_content(
-        model="gemini-2.5-flah",
-        contents=prompt
+        model="gemini-2.5-flash",
+        contents=prompt,
+        config=types.GenerateContentConfig(
+
+            response_mime_type="application/json",
+            response_schema=EmailClassification,
+        )
     )
     
+    extract_data = json.loads(response.text)
 
-    return {"destination_sector": response.destination_sector}
+    return {"destination_sector": extract_data["sector"]}
 
 # Node-2
 def  answer_finance(state: GraphState) -> dict:
@@ -55,3 +64,22 @@ def answer_support(state: GraphState) -> dict:
 # Node-4
 def answer_commercial(state:GraphState) -> dict:
     pass
+
+# TEST
+
+if __name__ == "__main__":
+
+    example_state = {
+
+    "sender_email": "cliente@email.com",
+    "body_email": "Olá, gostaria de saber o boleto da minha mensalidade que vence amanhã. Podem me enviar o código de barras?",
+    "destination_sector": None,
+    "final_answer": None
+    
+    }
+
+    print("Iniciando teste do nó de classificação...")
+
+    result = classify_email(example_state)
+
+    print("Resultado do Nó 1:", result)
